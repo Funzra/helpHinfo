e0f3ded33a22e446d661cee93dd03acc4761482b
Syoshiii <pirrello.enzo.pro@gmail.com>
2024-10-10 08:42:28 +0000

    Ajout HDD

--- helpInfo.sh
+++ helpInfo.sh
@@ -5,3 +5,7 @@ lsb_release -d | cut -f2- || cat /etc/os-release | grep PRETTY_NAME | cut -d '"'
 #### Free Memory
 echo -e "\n#### Free Memory"
 free -h | grep "Mem:" | awk '{print $4}'
+#### Disk Space
+echo -e "\n#### Disk Space"
+df -h / | awk 'NR==2 {printf "Free: %.2f GB /
+Total: %s\n", $4, $2}'
